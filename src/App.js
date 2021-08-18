15256b90df5aec70824588ac1cbd6e5333680261
soyuz53 <55592229+soyuz53@users.noreply.github.com>
2021-08-18 22:12:42 +0545

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner.js";
+import Nav from "./Nav";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Banner />
+
+      <Row
+        title="Netflix Originals"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
     </div>
   );
 }
---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "d9c6710fcd1d82e4d2bbeac1f0a5d635";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
